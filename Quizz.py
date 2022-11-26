b0ce6d3c134d20c399fb8fd71e27482102fff26b
LesQuatreY <tanguy.minot@laposte.net>
2022-11-26 16:41:58 +0100

    change main name file

--- Quizz.py
+++ Quizz.py
@@ -39,6 +39,7 @@ if ('random_list' in st.session_state) & (
                     st.write(f"ERROR : La préfécture est {jeu.Commune.lower()}")
                 else:
                     st.write("Bravo, c'est la bonne réponse.")
+    st.session_state["histo"] = jeu.historique
 
 rerun = st.checkbox("Voulez vous relancer ?")
 if rerun:
