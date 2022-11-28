a94b84732ede475ddfc64a147778b8a19f0dc6e6
LesQuatreY <tanguy.minot@laposte.net>
2022-11-28 15:14:44 +0100

    feat: nb value knows

--- pages/1_résultats.py
+++ pages/1_résultats.py
@@ -9,20 +9,18 @@ if "results" not in st.session_state:
 
 if st.session_state['results'].count(None)==0:
     st.write(f"Votre score est de {st.session_state['results'].count(True)}/{st.session_state['nb_tour']}.")
+    #Reprendre ses erreurs
+    code = st.radio(
+        label="Quel département voulez-vous revoir ?", options=[None]+st.session_state['random_list'],
+        horizontal = True
+        )
+    if code:
+        from Lancement_jeu import Jeu_Dpt
+        jeu = Jeu_Dpt()
+        jeu._graph(code)  
 else:
-    st.write("Veuillez finir le quizz pour afficher votre score.")
-
-#Reprendre ses erreurs
-code = st.radio(
-    label="Quel département voulez-vous revoir ?", options=[None]+st.session_state['random_list'],
-    horizontal = True
-    )
-if code:
-    from Lancement_jeu import Jeu_Dpt
-    jeu = Jeu_Dpt()
-    jeu._graph(code)   
-
-
+    st.write("Veuillez finir le quizz pour afficher votre score et revoir vos erreurs.")
+ 
 st.title("Affichage des statistiques")
 
 histo = st.session_state["histo"]
@@ -54,3 +52,10 @@ st.plotly_chart(
         title="Top du nombre de bonnes réponses"
     )
 )
+
+#Affichage du nombre de communes considérées comme acquises
+nb_acquis=histo[histo["Correct"]>histo["Erreur"]].shape[0]
+pct_acquis=nb_acquis/histo.shape[0]
+st.markdown(
+    "Nombre de communes acquises : {} ({:.0%})".format(nb_acquis, pct_acquis)
+    )
