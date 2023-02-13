10cf7238e7b282959112fa27ea9e2a938cbaacd6
LesQuatreY <tanguy.minot@laposte.net>
2023-02-13 15:32:41 +0100

    Added config file to visually format the page

---
+++ config.py
@@ -0,0 +1,40 @@
+# Fond sur la page principale
+ # Carte de France stylisée en jaune
+main_page_background = """
+[data-testid="stAppViewContainer"],
+[data-testid="stHeader"] {
+    background-image: url(
+        "https://static8.depositphotos.com/1260975/1037/v/450/depositphotos_10377644-stock-illustration-stylized-map-of-france.jpg"
+    ), linear-gradient(to right, #f5f5d0, #f5f5d0);
+    background-size: 35%, 100%;
+    background-position: 97% 0%, center;
+    background-repeat: no-repeat;
+}
+"""
+
+# Fond sur les pages secondaires
+ # Carte de France stylisée en jaune
+secondary_pages_background = """
+[data-testid="stAppViewContainer"],
+[data-testid="stHeader"] {
+    background-image: url(
+        "https://static8.depositphotos.com/1260975/1037/v/450/depositphotos_10377644-stock-illustration-stylized-map-of-france.jpg"
+    ), linear-gradient(to right, #f5f5d0, #f5f5d0);
+    background-size: 31%, 100%;
+    background-position: 97% 0%, center;
+    background-repeat: no-repeat;
+}
+"""
+
+# Fond avec la tour eiffel sur la sidebar
+sidebar_background = """
+[data-testid="stSidebar"]{
+    background-image: url(
+        "https://images.unsplash.com/photo-1511739001486-6bfe10ce785f?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8M3x8ZWlmZmVsJTIwdG93ZXJ8ZW58MHx8MHx8&w=1000&q=80"
+        );
+    background-size: 200%;
+    background-position: 50% 67%;
+    background-attachment: local;
+    background-repeat: no-repeat;
+}
+"""
