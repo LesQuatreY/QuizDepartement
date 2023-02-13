7dfac91946a00d72987458b9890fd4ac8c59e49b
LesQuatreY <tanguy.minot@laposte.net>
2023-02-13 15:37:44 +0100

    Add st.set_page_config() to configure page layout and appearance + graphics transparences

--- pages/2_Stats.py
+++ pages/2_Stats.py
@@ -1,26 +1,27 @@
+import config
 import streamlit as st
 import plotly.express as px
 
-st.markdown(
-    '<div align="center"> <h1 align="center">Affichage des statistiques</h1> </div>',
-    unsafe_allow_html=True
-    )
+# Configuration de la page
+st.set_page_config(
+    page_title="Statistiques",
+    layout="wide",
+    initial_sidebar_state="expanded",
+    page_icon="📊",
+    menu_items={
+    'Get Help': 'mailto:tanguy.minot@laposte.net',
+    'About': "Quizz by Tanguy Minot! 🧑‍💻"
+    }
+)
+
+#Affichage d'un titre
+st.title("📊 Statistiques")
+
+# Configuration avec des styles CSS pour les différents background
+st.markdown('<style>{}</style>'.format(config.secondary_pages_background), unsafe_allow_html=True)
+st.markdown('<style>{}</style>'.format(config.sidebar_background), unsafe_allow_html=True)
 
 histo = st.session_state["histo"]
-
-#Affichage des tops d'erreurs
-st.plotly_chart(
-    px.bar(
-        histo.assign(Commune=lambda x: x.Commune+" ("+x.index+")").sort_values(
-            ["Erreur", "Correct"], ascending=[False, True]
-            ).iloc[:5,:],
-        x="Commune", 
-        y="Erreur",
-        color_discrete_sequence=['#FF8181']*histo.shape[1],
-        labels={"Erreur":"Nombre d'erreurs"},
-        title="Top du nombre d'erreurs"
-    )
-)
 
 #Affichage des tops bonnes réponses
 st.plotly_chart(
@@ -33,7 +34,35 @@ st.plotly_chart(
         color_discrete_sequence=['#81FFA7']*histo.shape[1],
         labels={"Correct":"Nombre de bonnes réponses"},
         title="Top du nombre de bonnes réponses"
-    )
+    ).update_layout(
+        plot_bgcolor="rgba(0, 0, 0, 0)", 
+        paper_bgcolor="rgba(0, 0, 0, 0)",
+        xaxis_title=dict(font=dict(color='black')),
+        xaxis=dict(tickfont=dict(color='black'), title=dict(font=dict(color='black'))),
+        yaxis=dict(tickfont=dict(color='black'), title=dict(font=dict(color='black')))
+    ).update_traces(textposition='auto', textfont=dict(color='white')),
+    use_container_width=True
+)
+
+#Affichage des tops d'erreurs
+st.plotly_chart(
+    px.bar(
+        histo.assign(Commune=lambda x: x.Commune+" ("+x.index+")").sort_values(
+            ["Erreur", "Correct"], ascending=[False, True]
+            ).iloc[:5,:],
+        x="Commune", 
+        y="Erreur",
+        color_discrete_sequence=['#FF8181']*histo.shape[1],
+        labels={"Erreur":"Nombre d'erreurs"},
+        title="Top du nombre d'erreurs"
+    ).update_layout(
+        plot_bgcolor="rgba(0, 0, 0, 0)", 
+        paper_bgcolor="rgba(0, 0, 0, 0)",
+        xaxis_title=dict(font=dict(color='black')),
+        xaxis=dict(tickfont=dict(color='black'), title=dict(font=dict(color='black'))),
+        yaxis=dict(tickfont=dict(color='black'), title=dict(font=dict(color='black')))
+    ).update_traces(textposition='auto', textfont=dict(color='white')),
+    use_container_width=True
 )
 
 #Affichage du nombre de communes considérées comme acquises
